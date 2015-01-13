c6dd983e31dcb26403c01485f4258f3c606a196d
Diego B. Fernandez <diegobfernandez@outlook.com>
2015-01-13 02:40:36 -0200

    :fire: código da seção Testimonials removido

--- js/script.js
+++ js/script.js
@@ -8,23 +8,6 @@ $(window).load(function() {
   }
 });
 
-/* OWL Carousel */
-/*
-$(document).ready(function() {
-  var owl = $("#owl");
-  
-  owl.owlCarousel({
-    autoPlay: false,
-    items : 2, //10 items above 1000px browser width
-    itemsDesktop : [1000,2], //5 items between 1000px and 901px
-    itemsDesktopSmall : [900,2], // betweem 900px and 601px
-    itemsTablet: [767,1], //2 items between 600 and 0
-    itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-    pagination: true
-  });
-});
-*/
-
 /* Video library */
 $(document).ready(function () {
   $("#home").vide("video/video", {
