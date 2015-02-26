404fa00bd267fe219e65ffae3b7aad1696cac377
André Poleza <andrepoleza@nepente.io>
2015-02-26 13:38:51 -0300

    :+1: Adição de malarkey na seção de apresentação

---
+++ js/app.js
@@ -0,0 +1,26 @@
+(function() {
+  "use strict";
+
+  function m() {
+    var elemento = document.querySelectorAll('.malarkey')[0];
+
+    var opcoes = {
+      typeSpeed: 50,
+      deleteSpeed: 50,
+      pauseDelay: 1500,
+      loop: true,
+      postfix: ''
+    };
+
+    malarkey(elemento, opcoes)
+      .type('desenvolvendo').pause().delete()
+      .type('esculpindo').pause().delete()
+      .type('lapidando').pause().delete()
+      .type('tecendo').pause().delete()
+      .type('elaborando').pause().delete()
+      .type('confeccionando').pause().delete();
+  };
+
+  m();
+
+}());
