161080bc74b6c967985e03ee0861cbce59e09da1
Diego B. Fernandez <diegofernandez@nepente.io>
2015-04-24 02:00:58 -0300

    :bug: Correção do envio de email de contato

--- js/app.js
+++ js/app.js
@@ -25,11 +25,13 @@
     var frmContato = $('#frmContato');
     frmContato.on('submit', function(evt) {
       evt.preventDefault();
-      enviarEmail(frmContato.nome, frmContato.email, frmContato.assunto, frmContato.texto).done(function() {
-        clearFormElements(frmContato);
+      var form = frmContato[0];
+      enviarEmail(form.nome.value, form.email.value, form.assunto.value, form.texto.value).done(function(data) {
+        clearFormElements(form);
         $('#msgContato').text('Obrigado pelo contato. Em breve retornaremos seu e-mail.');
         $('#msgContato').show();
-      }).fail(function() {
+      }).fail(function(data) {
+        console.log(data);
         $('#msgContato').text('Houve um erro ao enviar o e-mail. Tente novamente mais tarde.');
         $('#msgContato').show();
       });
--- js/email.js
+++ js/email.js
@@ -1,24 +1,24 @@
 (function(global) {
+  var mandrillApiUrl = 'https://mandrillapp.com/api/1.0/messages/send.json';
+
   function enviarEmail(nomeDeOrigem, emailDeOrigem, assunto, texto) {
+    var mandrillMessageSendData = {
+      key: 'CmUGeHte5MR76JLu7uyOlg',
+      message: {
+        text: texto,
+        subject: assunto,
+        from_email: emailDeOrigem,
+        from_name: nomeDeOrigem,
+        to: [{ email: 'nepente@nepente.io' }]
+      }
+    };
+
     return $.ajax({
       type: 'POST',
-      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-      data: {
-        "key": "CmUGeHte5MR76JLu7uyOlg",
-        "message": {
-          "text": texto,
-          "subject": assunto,
-          "from_email": emailDeOrigem,
-          "from_name": nomeDeOrigem,
-          "to": [
-            {
-              "email": "nepente@nepente.io",
-              "name": "nepente",
-              "type": "to"
-            }
-          ]
-        }
-      }
+      url: mandrillApiUrl,
+      contentType: 'application/json;',
+      dataType: 'json',
+      data: JSON.stringify(mandrillMessageSendData)
     });
   }
 
