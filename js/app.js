475ab21e87001247aa203a35c4a25c8bc936eb98
Diego B. Fernandez <diegofernandez@nepente.io>
2015-05-27 04:42:36 -0300

    :ok_hand: Finaliza navegação alterando entre as âncoras

--- js/app.js
+++ js/app.js
@@ -1,25 +1,67 @@
 (function() {
   "use strict";
 
+  var lastId;
+  var topMenu = $('.top-bar');
+  var topMenuHeight = topMenu.outerHeight();
+  var menuItems = $('.top-bar-section', topMenu).find('a');
+
+  var scrollItems = menuItems.map(function() {
+    var item = $($(this).attr('href'));
+    if (item.length) { return item; }
+  });
+
+  menuItems.click(function(e) {
+    var href = $(this).attr('href'),
+    offsetTop = href === '#' ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+      scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+  });
+
+  $(window).scroll(function() {
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    var cur = scrollItems.map(function() {
+      if ($(this).offset().top < fromTop)
+        return this;
+    });
+
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : '';
+
+    if (lastId !== id) {
+      lastId = id;
+      menuItems
+      .parent().removeClass('active')
+      .end().filter('[href=#' + id + ']').parent().addClass('active');
+    }
+  });
+})();
+
+(function() {
+  "use strict";
+
   var isSmallScreen = !window.matchMedia('only screen and (min-device-width: 641px)').matches;
 
   function clearFormElements(ele) {
-	    $(ele).find(':input').each(function() {
-	        switch(this.type) {
-	            case 'password':
-	            case 'select-multiple':
-	            case 'select-one':
-	            case 'text':
-              case 'email':
-	            case 'textarea':
-	                $(this).val('');
-	                break;
-	            case 'checkbox':
-	            case 'radio':
-	                this.checked = false;
-	        }
-	    });
-	}
+    $(ele).find(':input').each(function() {
+      switch(this.type) {
+        case 'password':
+        case 'select-multiple':
+        case 'select-one':
+        case 'text':
+        case 'email':
+        case 'textarea':
+        $(this).val('');
+        break;
+        case 'checkbox':
+        case 'radio':
+        this.checked = false;
+      }
+    });
+  }
 
   function startEmail() {
     var frmContato = $('#frmContato');
